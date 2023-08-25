e66b46a8b1f3afc3bd9ca79ec5ab3969707c5772
AmitHegde3 <118654485+AmitHegde3@users.noreply.github.com>
2023-08-25 07:02:04 +0000

    Update compatibility with flask

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template, jsonify,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") 
 def home_page():
   return render_template('index.html')
 
-@app.route("/trade")
-def trade_page():
+@app.route("/")
+def test():
   return render_template('index.html')
+
+@app.route("/about_us")
+def about_us():
+  return render_template('about_page.html')
+
+@app.route("/trade_page")
+def trade_page():
+  return render_template('trade_page.html')
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, text
+import os
+
+#Do not use " " in seceret
+my_secret = os.environ['DB_CONNECTION_STRING']
+#this info is senstive never share it in github!!
+engine = create_engine(my_secret)
+
+with engine.connect() as conn:
+  print("\n\n---------------Connection Established!----------------\n\n")
+  result = conn.execute(text("select * from todo"))
+
+  li = []
+  for row in result.all():
+    result_dicts = {}
+    result_dicts["id"] = list(row)[0]
+    result_dicts["title"] = list(row)[1]
+    result_dicts["time"] = list(row)[2]
+    result_dicts["slaray"] = list(row)[3]
+    # print(list(row)[0])
+    li.append(result_dicts)
+
+  print(li)
+
+# def load_id(id):
+#   with engine.conn() as conn:
+#     result = conn.execute(text("select * from todo where id= :val"),val=id)
+#     rows = result.all()
+#     if(len(rows)==0):
+#       return None
+#     else:
+#       return dict(rows[0])
+
+
+def add_todo_db(data):
+  
+  with engine.connect() as conn:
+    print("\n\nData is:\n\n", data)
+    print("\n\n")
+    query = text(
+      "INSERT INTO todo(title,time,slaray) values(:title ,:time ,:salary)")
+    conn.execute(
+      query, dict(title=data['title'],
+                  time=data['time'],
+                  salary=data['salary']))
+   
+    conn.commit()
+    print("\n\n----Updated DB-----\n\n")
