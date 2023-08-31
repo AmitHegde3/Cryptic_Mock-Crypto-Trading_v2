d11d4552c87688c4ad6b3648b923495ed44634fc
AmitHegde3 <118654485+AmitHegde3@users.noreply.github.com>
2023-08-31 18:31:19 +0000

    Update register page and trade page popup

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
 from flask import Flask, render_template, jsonify,request
+Coins = [
+  {
+    'coin_id':'BTC',
+    'name':'Bitcoin',
+    'price':'2340000',
+    'rate':'1.12%'
+  },
+  {
+    'coin_id':'ETH',
+    'name':'Etherum',
+    'price':'146000',
+    'rate':'5.56%'
+  },
+  {
+    'coin_id':'DOGE',
+    'name':'DOGE Coin',
+    'price':'5.56 INR',
+    'rate':'6.7%'
+  },
+  {
+    'coin_id':'SHIB',
+    'name':'SHIBHA INU',
+    'price':'0.0045',
+    'rate':'3.45%'
+  },
 
+
+  
+]
 app = Flask(__name__)
 
 @app.route("/") 
@@ -16,7 +44,11 @@ def about_us():
 
 @app.route("/trade_page")
 def trade_page():
-  return render_template('trade_page.html')
+  return render_template('trade_page.html',coin=Coins)
+@app.route("/register")
+
+def register():
+  return render_template('register_page.html')
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
